f1427a73879deda334ad3fcf60ca32d3bb2c29c3
jonajosejg <rojii0xff@gmail.com>
2025-03-13 19:33:56 -0700

    minimize: minor

--- minimization/minimize.py
+++ minimization/minimize.py
@@ -2,7 +2,11 @@ import os
 from amplpy import AMPL, add_to_path
 from pathlib import Path
 
-# Helper Functions
+
+#Helper fuctions
+def AMPLDIR():
+    homedir = os.path.expanduser('~')
+    return os.path.join(homedir, 'AMPL')
 
 def get_csv_data(filename):
     if not filename.endswith('.csv'):
@@ -19,3 +23,8 @@ def get_ampl_data(filename):
     # Expand the user's home directory and construct the full path
     homedir = os.path.expanduser('~')
     return os.path.join(homedir, 'final/test/data', filename)
+
+
+# AMPL Interface
+add_to_path(AMPLDIR())
+ampl = AMPL()
